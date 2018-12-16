de33ea9c846648576cf034e6ba8b104144fdb512
Deepak Prakash <deepakprakash2811@gmai.com>
2018-12-16 19:55:24 +0530

    modified pages

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class home extends StatelessWidget {
+final materialApp = MaterialApp(
+
+  debugShowCheckedModeBanner: false,  // Title
+      title: "FlutJob",
+      // Home
+      home: new Scaffold(
+        // Appbar
+        appBar: new AppBar(
+          // Title
+          title: new Text("Home"),
+        ),
+        // Body
+        body: new home()
+      ));
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("data"),
-      
-    );
+    return materialApp;
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,5 @@ import 'home.dart';
 
 void main() {
   
-  runApp(new MaterialApp(
-  debugShowCheckedModeBanner: false,  // Title
-      title: "FirstPage",
-      // Home
-      home: new Scaffold(
-        // Appbar
-        appBar: new AppBar(
-          // Title
-          title: new Text("FlutJob"),
-        ),
-        // Body
-        body: new home()
-      )));
+  runApp(home());
 }
