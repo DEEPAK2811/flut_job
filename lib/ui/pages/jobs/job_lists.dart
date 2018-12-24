025d725c0869b43f72a4669f402c305b4e7ed016
Deepak Prakash <deepakprakash2811@gmai.com>
2018-12-24 20:06:26 +0530

    joblist

---
+++ lib/ui/pages/jobs/job_lists.dart
@@ -0,0 +1,34 @@
+import 'package:flut_job/models/joblistmodel.dart';
+import 'package:flutter/material.dart';
+
+class joblist extends StatefulWidget {
+  @override
+  _joblistState createState() => _joblistState();
+}
+
+class _joblistState extends State<joblist> {
+  List<Joblistmodel> joblists = new List();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: new ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return new Card(
+          child: new Column(
+            children: <Widget>[
+              new Text(joblists[0].jobid),
+              new Text(joblists[1].jobname),
+              new Text(joblists[2].jobdescription),
+              new Text(joblists[3].joblocation),
+              new Text(joblists[4].jobamount),
+              new Text(joblists[5].jobtime),
+            ],
+          ),
+        );
+      },
+      itemCount: joblists == null ? 0 : joblists.length,
+    ));
+  }
+
+}
