2c2ff9be42521694ec897b34d0d0f76de6e85553
Deepak Prakash <deepakprakash2811@gmai.com>
2019-01-18 02:36:55 +0530

    account drawer updated

--- lib/models/navigation_bar.dart
+++ lib/models/navigation_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flut_job/utils/uidata.dart';
 import 'package:flut_job/ui/pages/testpage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class NavBar extends StatelessWidget {
 
@@ -12,22 +13,30 @@ class NavBar extends StatelessWidget {
       child: ListView(
         children: <Widget>[
           new UserAccountsDrawerHeader(
-            accountName: new Text("Deepak Prakash"),
-            accountEmail: new Text("Deepakprakash2811@gmail.com"),
+            accountName: new Text("Deepak Prakash",style:TextStyle(color: Colors.white),),
+            accountEmail: new Text("Deepakprakash2811@gmail.com",style:TextStyle(color: Colors.white),),
+            currentAccountPicture: new CircleAvatar(
+              backgroundImage: AssetImage("assets/ag.jpg"),
+              radius: 40,
+            ),
+             decoration: BoxDecoration(image: new DecorationImage(
+               image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover
+             )),
+          ),
 
-          ),
           new ListTile(
-            title: new Text("Jobs Assigned"),
-            onTap: ()=> Navigator.popAndPushNamed(context, "/")
-          ),
-          new ListTile(
-            title: new Text("Test 2 "),
+            title: new Text("Home "),
             onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
             ),
-          
-          new Divider(),
           new ListTile(
-            title: new Text("Test 3"),
+            title: new Text("Jobs Assigned"),
+            onTap: () => Navigator.pushNamed(context, UIData.assignJobRoute)
+          ),
+          
+          new Divider(indent: 10.0,),
+          new ListTile(
+            title: new Text("Sign Out"),
+           
           )
         ],
 
